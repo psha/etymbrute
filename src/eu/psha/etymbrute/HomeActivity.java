212d425c54dac37cabe8cddec26ecc5bc309d371
psha <no>
2014-04-07 18:37:21 +0200

    gui start

--- src/eu/psha/etymbrute/HomeActivity.java
+++ src/eu/psha/etymbrute/HomeActivity.java
@@ -3,6 +3,7 @@ package eu.psha.etymbrute;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.SearchView;
 
 public class HomeActivity extends Activity {
 
@@ -15,7 +16,13 @@ public class HomeActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
+		getMenuInflater().inflate(R.menu.options_menu, menu);
+		getActionBar().setDisplayShowTitleEnabled(false);
+		
+		//Keep the search bar open (not minimised to a magnifying glass icon)
+		SearchView sw = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+		sw.setIconifiedByDefault(false);
+		
 		return true;
 	}
 
