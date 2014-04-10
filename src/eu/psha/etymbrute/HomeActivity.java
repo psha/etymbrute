c3262d8b288777826beb43ccb474cb83228aa9a2
svar <svar@nr4>
2014-04-10 11:35:03 +0200

    search suggest

--- src/eu/psha/etymbrute/HomeActivity.java
+++ src/eu/psha/etymbrute/HomeActivity.java
@@ -2,15 +2,31 @@ package eu.psha.etymbrute;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.SearchView;
 
 public class HomeActivity extends Activity {
 
+
+	String tag = "EtymBrute";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+	    Intent intent = getIntent();
+	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+	      String query = intent.getStringExtra(SearchManager.QUERY);
+	      Log.d(tag, "onCreate() caugth search");
+	      doSearch(query);
+	    }
+
+		
 	}
 
 	@Override
@@ -18,12 +34,41 @@ public class HomeActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.options_menu, menu);
 		getActionBar().setDisplayShowTitleEnabled(false);
-		
-		//Keep the search bar open (not minimised to a magnifying glass icon)
-		SearchView sw = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+
+		// Keep the search bar open (not minimised to a magnifying glass icon)
+		SearchView sw = (SearchView) menu.findItem(R.id.menu_search)
+				.getActionView();
 		sw.setIconifiedByDefault(false);
+
+		// TODO: move icon closer to app icon, does this really work, feels
+		// hacky
+		sw.setPadding(-16, 0, 0, 0);
+
+		  // Get the SearchView and set the searchable configuration
+	    SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+	    SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+	    // Assumes current activity is the searchable activity
+	    searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
 		
 		return true;
 	}
-
+	
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // Because this activity has set launchMode="singleTop", the system calls this method
+        // to deliver the intent if this activity is currently the foreground activity when
+        // invoked again (when the user executes a search from this activity, we don't create
+        // a new instance of this activity, so the system delivers the search intent here)
+    	//
+    	// The emulator has a bug, this is called twice every time. Should work on actual devices.
+	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+		      String query = intent.getStringExtra(SearchManager.QUERY);
+		      Log.d(tag, "onNewIntent() caugth search");
+		      doSearch(query);
+		    }
+    }
+    
+    public void doSearch(String query){
+	      Log.d(tag, "Handle query: " + query);
+    }
 }
---
+++ src/eu/psha/etymbrute/WordProvider.java
@@ -0,0 +1,48 @@
+package eu.psha.etymbrute;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+public class WordProvider extends ContentProvider {
+
+	@Override
+	public int delete(Uri arg0, String arg1, String[] arg2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getType(Uri arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri arg0, ContentValues arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCreate() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		Log.d("EtymBrute", ".query() Uri= " + uri.toString());
+		return null;
+	}
+
+	@Override
+	public int update(Uri arg0, ContentValues arg1, String arg2, String[] arg3) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
